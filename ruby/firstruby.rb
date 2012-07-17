498467752c146dca198587414d4fefb2dc7bcf0d
Pemba Sherpa <pemsher@gmail.com>
2012-07-17 16:16:19 -0700

    location prompt added

--- ruby/firstruby.rb
+++ ruby/firstruby.rb
@@ -2,4 +2,6 @@ print "What is your name?"
 name = gets.chomp
 print "How old are you?"
 age = gets.chomp
-print "Hello " + name + "! Your are " + age + " years old."
+print "Where you from?"
+location = gets.chomp
+print "Hello " + name + "! Your are " + age + " years old, and you are from " + location
